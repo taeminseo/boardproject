578e2db2e3ecaae398fc4ddc5b9dd3aee8f21539
taeminseo <taeminseo@naver.com>
2023-01-28 18:31:02 +0900

    #17 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 f47e95a 에서 확인했기 때문에 , 나머지 뷰도 동일하게 작동할 것을 예상하고 
    미리 테스트 메소드 업데이트

--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -44,7 +44,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -59,7 +59,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/serarch"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -72,7 +72,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/serarch-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 
